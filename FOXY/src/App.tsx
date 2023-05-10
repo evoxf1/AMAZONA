a404e67252029698e12824ee5cf719469f2829ee
evoxf1 <sahil2812@live.com>
2023-05-10 14:18:02 +0530

    Please commit
    	# new file:   FOXY/public/vite.svg

---
+++ FOXY/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>FOXY!</header>
+      <main></main>
+      <footer>All Rights Reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
