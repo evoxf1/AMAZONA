701ae6dfb709a6c105df404c2eb17834221f5847
evoxf1 <sahil2812@live.com>
2023-05-11 15:19:10 +0530

    commit it fast

--- FOXY/src/main.tsx
+++ FOXY/src/main.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import React from "react";
+import ReactDOM from "react-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.tsx";
+import "./index.css";
+import HomePage from "./pages/HomePage.tsx";
+import ProductPage from "./pages/ProductPage.tsx";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index={true} element={<HomePage />} />
+          <Route path="product/:slug" element={<ProductPage />} />
+          {/* <Route path="dashboard" element={<Dashboard />} /> */}
+          {/* ... etc. */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
