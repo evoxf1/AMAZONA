4182d22b987a0654c1d7ed409b6ec973e6c5a28c
evoxf1 <sahil2812@live.com>
2023-05-15 09:13:06 +0530

    now

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,43 @@
 import cors from "cors";
-import express, { Request, Response } from "express";
-import { sampleProducts } from "./data";
+import dotenv from "dotenv";
+import express from "express";
+// import { sampleProducts } from "./data";
+import mongoose from "mongoose";
+import { productRouter } from "./routers/productRouter";
+import seedRouter from "./routers/seedRouter";
 
+dotenv.config();
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/foxydb";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch(() => {
+    console.log("error mongodb");
+  });
 
 const app = express();
 
 // Enable CORS for all origins
-app.use(cors({
-  credentials:true,
-  origin:['http://localhost:5173']
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173"],
+  })
+);
 
-app.get("/api/products", (req, res) => {
-  res.json(sampleProducts);
-});
+// app.get("/api/products", (req, res) => {
+//   res.json(sampleProducts);
+// });
 
-app.get('/api/products/:slug', (req: Request, res: Response) => {
-  res.json(sampleProducts.find((x) => x.slug === req.params.slug))
-})
+// app.get("/api/products/:slug", (req: Request, res: Response) => {
+//   res.json(sampleProducts.find((x) => x.slug === req.params.slug));
+// });
+app.use("/api/products", productRouter);
+app.use("/api/seed", seedRouter);
+
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
